a50289fe73153eabcbbdac89b9779c2b2a33a9ea
sahil <shaneamgarcia@gmail.com>
2022-09-04 10:37:42 +0200

    fix variable name

--- Filter/calculatefilterscore.py
+++ Filter/calculatefilterscore.py
@@ -27,26 +27,26 @@ def parse_args():
     return args
 
 def process_data(d):
-                                                                                                           
-            #filtering data having triple size less than 6 
-                                                                            
-            if len(d['triples']) < 6:                                                                                       
-                gleu_score = google_bleu.compute(predictions=[d['serialized_triples']],references = [[d['text']]])          
-                bleu_score = sacrebleu.corpus_bleu([d['serialized_triples']],[[d['text']]]).score                           
-                gleu_score= round(gleu_score["google_bleu"], 2)                                                             
-                d['gleu'] = gleu_score                                                                                      
-                d['bleu'] = bleu_score                                                                                      
-                #triplesetgleu[len(d['triples'])].append(gleu_score)                                                         
-                #triplesetbleu[len(d['triples'])].append(bleu_score)                                                         
-                #triplesetsimilarity[len(d['triples'])].append(d['similarity'])                                              
+
+            #filtering data having triple size less than 6
+
+            if len(d['triples']) < 6:
+                gleu_score = google_bleu.compute(predictions=[d['serialized_triples']],references = [[d['text']]])
+                bleu_score = sacrebleu.corpus_bleu([d['serialized_triples']],[[d['text']]]).score
+                gleu_score= round(gleu_score["google_bleu"], 2)
+                d['gleu'] = gleu_score
+                d['bleu'] = bleu_score
+                #triplesetgleu[len(d['triples'])].append(gleu_score)
+                #triplesetbleu[len(d['triples'])].append(bleu_score)
+                #triplesetsimilarity[len(d['triples'])].append(d['similarity'])
                 return gleu_score,bleu_score,d
             else:
                 return None
-                #bleuStats.append(bleu_score)                                                                                
-                #json.dump(d, outfile)                                                                                       
+                #bleuStats.append(bleu_score)
+                #json.dump(d, outfile)
                 #outfile.write('\n')
-                #outfile.flush()                                                                                         
-                                               
+                #outfile.flush()
+
 
 
 
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     args = parse_args()
     with jsonlines.open(args.input_path) as reader:
 
-      with open(args.save_path, 'w') as outfile: 
+      with open(args.save_path, 'w') as outfile:
         pool = Pool(multiprocessing.cpu_count())                         # Create a multiprocessing Pool
         for result in pool.imap(process_data, reader):
             if result:
@@ -66,7 +66,7 @@ if __name__ == '__main__':
              triplesetbleu[len(result[2]['triples'])].append(result[1])
              triplesetsimilarity[len(result[2]['triples'])].append(result[2]['similarity'])
              json.dump(result[2], outfile)
-             outfile.write('\n')                                                                                       
+             outfile.write('\n')
 
 
 
@@ -81,4 +81,4 @@ for k in range(1,6):
   #print(f'bleu after {i} : {bleuafter[i]}')
  print(f'bleu for size {k} : {np.mean(triplesetbleu[k])}')
  print(f'similarity for size {k} : {np.mean(triplesetsimilarity[k])}')
- print(f'gleu for size {k} : {np.mean(triplesetsimilarity[k])}')
+ print(f'gleu for size {k} : {np.mean(triplesetgleu[k])}')
