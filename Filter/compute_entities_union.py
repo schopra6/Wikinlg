08c469b6f51c1321a1198cb50d3722f5b4b5ddb6
sahil <shaneamgarcia@gmail.com>
2022-09-19 10:53:24 +0200

    add requirements.txt file

--- Filter/compute_entities_union.py
+++ Filter/compute_entities_union.py
@@ -1,11 +1,62 @@
 import argparse
+import pandas as pd
+import itertools
+import numpy as np
+
+def uniq(lst):
+    last = object()
+    for item in lst:
+        if item == last:
+            continue
+        yield item
+        last = item
+
+def sort_and_deduplicate(l):
+    return list(uniq(sorted(l, reverse=True)))
 def parse_args():
     parser = argparse.ArgumentParser(description='Split the noisy test dataset.')
-    parser.add_argument('--input-path', type=str,
-                        help='path to the dataset')
+    parser.add_argument('--noisy-dataset-path', type=str,
+                        help='path to the noisy test dataset')
     parser.add_argument('--save-path', type=str,
-                        help='path to save the  dataset')
-    parser.add_argument('--save-filename', type=str,
-                        help='filename to save the  dataset')
+                        help='path to save the split dataset')
+
     args = parser.parse_args()
     return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    wb_df = pd.read_csv(args.noisy_dataset_path)
+
+    wb_entities = list(map(lambda x: list(map(lambda y: [y[0], y[2]], eval(x))), wb_df['triples']))
+    wb_entities = list(itertools.chain(*wb_entities))
+    wb_entities = list(itertools.chain(*wb_entities))
+
+    wb_entities = set(wb_entities)
+
+    wb_props = list(map(lambda x: list(map(lambda y: y[1], eval(x))), wb_df['triples']))
+    wb_props = list(itertools.chain(*wb_props))
+    wb_props = set(wb_props)
+
+    wb_triples = list(map(lambda x: eval(x), wb_df['triples']))
+    wb_triples = list(itertools.chain(*wb_triples))
+    wb_triples = sort_and_deduplicate(wb_triples)
+    #wb_triples = list(itertools.chain(*wb_triples))
+    #wb_triples = set(wb_triples)
+    wb_graph = list(map(lambda x: eval(x), wb_df['triples']))
+    print(wb_graph[0])
+    wb_graph = sort_and_deduplicate(wb_graph)
+    print(f'properties {len(wb_props)} entities {len(wb_entities)} triples {len(wb_triples)} graph {len(wb_graph)}')
+    with open(args.save_path +'/noisy_properties.txt', 'w') as f:
+        for line in wb_props:
+            f.write(f"{line}\n")
+    with open(args.save_path +'/noisy_entities.txt', 'w') as f:
+        for line in wb_entities:
+            f.write(f"{line}\n")
+    with open(args.save_path +'/noisy_triples.txt', 'w') as f:
+        for line in wb_triples:
+            f.write(f"{str(line)}\n")
+    with open(args.save_path +'/noisy_graph.txt', 'w') as f:
+        for line in wb_graph:
+            f.write(f"{str(line)}\n")
--- simtestdata/split_noisy_val_dataset.py
+++ simtestdata/split_noisy_val_dataset.py
@@ -3,7 +3,16 @@ import pandas as pd
 import itertools
 import numpy as np
 
+def uniq(lst):
+    last = object()
+    for item in lst:
+        if item == last:
+            continue
+        yield item
+        last = item
 
+def sort_and_deduplicate(l):
+    return list(uniq(sorted(l, reverse=True)))
 def parse_args():
     parser = argparse.ArgumentParser(description='Split the noisy test dataset.')
     parser.add_argument('--noisy-dataset-path', type=str,
@@ -40,9 +49,17 @@ if __name__ == '__main__':
     wb_triples = list(itertools.chain(*wb_triples))
     #wb_triples = list(itertools.chain(*wb_triples))
     #wb_triples = set(wb_triples)
-    
+    wb_graph = list(map(lambda x: eval(x), wb_df['triples']))
+    print(wb_graph[0])
+    wb_graph = sort_and_deduplicate(wb_graph)
+    property_list = []
+    entities_list = []
+    triples_list = []
+    graph_list = []
+
     
     my_df = pd.read_csv(args.noisy_dataset_path)
+    my_df_copy = my_df
     my_df['unknown_properties'] = ['No'] * len(my_df)
     my_df['unknown_entities'] = ['No'] * len(my_df)
     my_df['unknown_triples'] = ['No'] * len(my_df)
@@ -55,21 +72,39 @@ if __name__ == '__main__':
         for trip in triples:
             if trip[1] in wb_props:
                 known_properties = True
+                property_list.append(trip[1])
             if trip[0] in wb_entities or trip[2] in wb_entities:
                 known_entities = True
+                entities_list.append(trip[0])
+                entities_list.append(trip[2])
             if trip in wb_triples:
                 known_triples = True
+                triples_list.append(trip)
+        if triples in wb_graph:
+            graph_list.append(triples)
+
         if not known_entities:
-            my_df.iloc[i, 3] = "Yes"
-        if not known_properties:
-            my_df.iloc[i, 2] = "Yes"
-        if not known_triples:
             my_df.iloc[i, 4] = "Yes"
-            
-    my_df = my_df[(my_df['sentence'].str.len() > 50) & (my_df['sentence'].str.len() < 250)]
-    my_df = my_df[np.array(list(map(lambda x: len(eval(x)), my_df['triples'].values))) <= 5]
-    
-    my_df_props = my_df[my_df['unknown_properties'] == 'Yes']
+        if not known_properties:
+            my_df.iloc[i, 3] = "Yes"
+        if not known_triples:
+            my_df.iloc[i, 5] = "Yes"
+    print(f'properties {len(property_list)} entities {len(entities_list)} triples {len(triples_list)} graph {len(graph_list)}')
+    with open('properties.txt', 'w') as f:
+        for line in property_list:
+            f.write(f"{line}\n")
+    with open('entities.txt', 'w') as f:
+        for line in entities_list:
+            f.write(f"{line}\n")
+    with open('triples.txt', 'w') as f:
+        for line in triples_list:
+            f.write(f"{str(line)}\n")
+    with open('graph.txt', 'w') as f:
+        for line in graph_list:
+            f.write(f"{str(line)}\n")
+    my_df_trim = my_df[(my_df['sentence'].str.len() > 50) & (my_df['sentence'].str.len() < 250)]
+   # my_df = my_df[np.array(list(map(lambda x: len(eval(x)), my_df['triples'].values))) <= 5]
+    my_df_props = my_df_trim[my_df_trim['unknown_properties'] == 'Yes']
     num_triples_props = np.array(list(map(lambda x: len(eval(x)), my_df_props['triples'].values)))
 
     mask_props_by_triples_num = [num_triples_props == i for i in range(1, 6)]
@@ -81,12 +116,12 @@ if __name__ == '__main__':
         
     my_df_props[mask_props].to_csv(f"{args.save_path}/no_props_{args.language_code}.csv", index=False)
     my_df.drop(my_df_props[mask_props].index).to_csv(f"{args.save_path}/train_no_props_{args.language_code}.csv", index=False)
-
+    my_df_copy = my_df_copy.drop(my_df_props[mask_props].index,errors='ignore')
     #x = my_df.drop(mask_props, axis=0,inplace=True)
     #x.to_csv(f"{args.save_path}/train_no_props_{args.language_code}.csv", index=False)
     
     
-    my_df_ents = my_df[my_df['unknown_entities'] == 'Yes']
+    my_df_ents = my_df_trim[my_df_trim['unknown_entities'] == 'Yes']
     num_triples_ents = np.array(list(map(lambda x: len(eval(x)), my_df_ents['triples'].values)))
 
     mask_ents_by_triples_num = [num_triples_ents == i for i in range(1, 6)]
@@ -99,10 +134,10 @@ if __name__ == '__main__':
     my_df_ents[mask_ents].to_csv(f"{args.save_path}/no_ents_{args.language_code}.csv", index=False)
     my_df.drop(my_df_ents[mask_ents].index).to_csv(f"{args.save_path}/train_no_ents_{args.language_code}.csv", index=False)
     #my_df_trips[np.logical_not(mask_trips)].to_csv(f"{args.save_path}/train_no_trips_{args.language_code}.csv", index=False)
-
+    my_df_copy = my_df_copy.drop(my_df_ents[mask_ents].index,errors='ignore')
     
     
-    my_df_trips = my_df[my_df['unknown_triples'] == 'Yes']
+    my_df_trips = my_df_trim[my_df_trim['unknown_triples'] == 'Yes']
     num_triples_trips = np.array(list(map(lambda x: len(eval(x)), my_df_trips['triples'].values)))
 
     mask_trips_by_triples_num = [num_triples_trips == i for i in range(1, 6)]
@@ -114,5 +149,6 @@ if __name__ == '__main__':
         
     my_df_trips[mask_trips].to_csv(f"{args.save_path}/no_trips_{args.language_code}.csv", index=False)
     my_df.drop(my_df_trips[mask_trips].index).to_csv(f"{args.save_path}/train_no_trips_{args.language_code}.csv", index=False)
+    my_df_copy.drop(my_df_trips[mask_trips].index,errors='ignore').to_csv(f"{args.save_path}/train_no_{args.language_code}.csv", index=False)
 
 
